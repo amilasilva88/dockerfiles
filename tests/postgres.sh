748e1114ef4e82f410badb75aad4e1e3936f6958
Fernando Ike <fike@midstorm.org>
2015-11-21 00:11:02 -0200

    Added script to generate postgresql dockerfiles

--- tests/postgres.sh
+++ tests/postgres.sh
@@ -1,23 +1,42 @@
 #!/bin/bash
 
-build(){
-for i in $(ls -1 ~/dockerfiles/postgres |egrep "[0-9]{1,2}\.[0-9]{1,2}.*") 
-do 
-   if [[ -e ~/container/$i.tar ]] 
-     then docker load --input ~/container/$i.tar 
-     fi
-       docker build --rm -t fike/postgres:$i ~/dockerfiles/postgres/$i 
-       mkdir -p ~/container 
-       docker save --output ~/container/$i.tar fike/postgres:$i 
+RUN=()
+CONTAINERS=0
+VERSIONS=$(ls -1 ~/dockerfiles/postgres |egrep "[0-9]{1,2}\.[0-9]{1,2}.*"|sort)
+
+for i in $(echo $VERSIONS)
+do
+  if [ $(($CONTAINERS % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX ]
+  then
+    RUN+=" $i"
+    fi
+  ((CONTAINERS=CONTAINERS+1))
+
 done
+
+
+
+build(){
+
+for y in $(echo ${RUN[@]})
+do
+  if [[ -e ~/container/$y.tar ]]
+  then 
+    docker load --input ~/container/$y.tar
+   fi
+      docker build --rm -t fike/postgres:$y ~/dockerfiles/postgres/$y 
+      mkdir -p ~/container 
+      docker save --output ~/container/$y.tar fike/postgres:$y
+done
+
 }
 
 run_test(){
-for i in $(ls -1 ~/dockerfiles/postgres | egrep "[0-9]{1,2}\.[0-9]{1,2}.*"); 
-  do docker run -d --name $i fike/postgres:$i && 
-    docker run -it --net="container:$i" -e PGPASSWORD="foobar" fike/postgres:$i  \
-      psql -h 127.0.0.1 -p 5432 -U postgres --command "SELECT version();" 
-    docker stop $i; done
+for y in $(echo ${RUN[@]}); 
+  do docker run -d --name $y fike/postgres:$y && 
+    docker run -it --net="container:$y" -e PGPASSWORD="foobar" fike/postgres:$y  \
+    psql -h 127.0.0.1 -p 5432 -U postgres --command "SELECT version();" 
+    docker stop $y; done
 }
 
 case "$1" in
